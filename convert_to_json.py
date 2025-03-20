2c9bb45d3d2244f077903816dab9ebf0e1e88858
Dolapo Olatoye <dolaposalim@gmail.com>
2025-03-20 11:48:12 +0100

    Refactor CSV file handling and update output paths for JSON conversion

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv(r"catami-classification\catami\catami-caab-codes.csv", delimiter=",")  # Ensure the delimiter matches your file
-
+file_path = input('Enter the file path remove quotes: ')  # Achalugo remove the quotes before pressing enter
+df = pd.read_csv(file_path, delimiter=",", encoding="ISO-8859-1")  # Ensure the delimiter matches your file
 json_output = df.to_json(orient="records", indent=4)
-
-with open(r"catami-classification\catami-caab.json2", "w") as f:
+output_path = input('Enter the path to save directory: ')
+output_file_path = output_path + "\\convertfileinjson.json"
+with open(output_file_path, "w") as f:
     f.write(json_output)
-
 print(json_output)
